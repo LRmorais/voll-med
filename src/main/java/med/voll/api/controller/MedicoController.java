6a1c94901f939048d71ef396e55e2f4dfffa34a0
Lucas Morais <lucas.morais23ti@gmail.com>
2023-06-11 19:22:32 -0300

    rota para listagem de medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,12 +3,14 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -24,7 +26,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<Medico> listar() {
-        return repository.findAll();
+    public Page<DadosListagemMedicos> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedicos::new);
     }
 }
